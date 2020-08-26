ca65c052d17139c6bd8a98b92f9cf37438c3732f
Ju A Han <jane.han13@gmail.com>
2020-08-25 21:08:25 -0600

    Initial commit

---
+++ app.py
@@ -0,0 +1,8 @@
+# import sqlalchemy
+# from sqlalchemy.ext.automap import automap_base
+# from sqlalchemy.orm import Session
+# from sqlalchemy import create_engine, func
+# import itertools
+# from operator import itemgetter
+
+from flask import Flask, jsonify, render_template
